bbb37c0e2d72ac6637ed1260fb5a5f0edd39d6e4
omcewan <orlandomcewan231@gmail.com>
2022-01-12 17:43:30 -0500

    added functionality

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 function getRepoIssues(repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -6,6 +7,11 @@ function getRepoIssues(repo) {
     if (response.ok) {
       response.json().then((data) => {
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("Link")) {
+          displayWarning(repo)
+        }
       });
     } else {
       alert("There was a problem with your request");
@@ -47,4 +53,17 @@ function displayIssues(issues) {
     issueContainerEl.appendChild(issueEl);
   }
 }
-getRepoIssues("omcewan/run-buddy");
+
+function displayWarning(repo) {
+  // add text to warning container
+  limitWarningEl.textContent = "To see more than 30 issues, visit ";
+  var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+}
+
+getRepoIssues("facebook/react");
